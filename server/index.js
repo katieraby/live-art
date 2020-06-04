c71952cd6b85dea86ab9a0338831c2487da4540d
Katie <50029859+katieraby@users.noreply.github.com>
2020-06-04 16:31:13 +0100

    Changes for build

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.use(express.json());
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + "../build"));
 
 app.get("/", (req, res, next) => res.sendFile(__dirname + "./index.html"));
 
