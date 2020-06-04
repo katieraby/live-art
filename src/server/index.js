a6ece46eea594a9a3828f03347ddd619068c20b9
Katie <50029859+katieraby@users.noreply.github.com>
2020-06-04 19:06:01 +0100

    Reverted to previous

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,10 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-const path = require("path");
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "build")));
+app.use(express.static("build"));
 
+const path = require("path");
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
