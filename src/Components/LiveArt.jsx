f56fc42782c993157cf8bdba30432f89368785c6
Katie <50029859+katieraby@users.noreply.github.com>
2020-06-04 17:55:00 +0100

    Added view live art link

--- src/Components/LiveArt.jsx
+++ src/Components/LiveArt.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "@reach/router";
 
-const LiveArt = () => {
+const LiveArt = ({ username }) => {
   //populate live art list with artists array
   //use another context of user logged in = true to enable the user actuallly click on the list items
+
   return (
     <div>
       <p>Live Art List</p>
+      <button>
+        <Link to={`/${username}`}>View Live Art</Link>
+      </button>
     </div>
   );
 };
