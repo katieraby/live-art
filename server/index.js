91e03e33c38e2a26975ec9eb2cffad4c26769c00
Katie <50029859+katieraby@users.noreply.github.com>
2020-06-04 16:29:59 +0100

    Changes for build

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/build"));
+
+app.get("/", (req, res, next) => res.sendFile(__dirname + "./index.html"));
 
 io.on("connection", (socket) => {
   socket.emit("messageFromServer", { data: "welcome to the io server" });
